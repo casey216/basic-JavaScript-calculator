af0bb2048f3cedc72d4c15bea582ddeb45de3f1d
casey216 <caseynzewi@gmail.com>
2022-06-24 19:07:17 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,55 +1,79 @@
-alert("Hello")
-#main function
-def main():
-    print("welcome to Simple CLI calculator")
+// main function
+function main() {
+    alert("welcome to Simple CLI calculator");
 
-    #collects user input
-    number1 = validate_number(input("Enter the first number: "))
-    number2 = validate_number(input("Enter the second number: "))
-    operand = validate_operand()
+    //collects user input
+    let number1 = validate_number(prompt("Enter the first number: "));
+    if (number1 != false) {
+		let number2 = validate_number(prompt("Enter the second number: "));
+		if (number2 != false) {
+			let operand = validate_operand();
 
-    #calculates and prints answer
-    ans = calculate(number1, number2, operand)    
-    print(f"{number1} {operand} {number2} = {ans}")
+			//calculates and prints answer
+			if (operand != false) {
+				let ans = calculate(number1, number2, operand);
+				alert(number1 + " " + operand + " " + number2 + " = " + ans);
+			}
+		}
+	}
+}
 
-#fuction to validate numbers
-def validate_number(n):
-    try:
-        n = int(n)
-        return n
-    except:
-        print(f"your input {n} is not an integer!!!")
-        validate_number(input("Enter a valid number: "))
+//function to validate numbers
+function validate_number(n) {
+    if (isNaN(n)) {
+		return validate_number(window.prompt("Enter a valid number: "));
+	}
+	else if (n == null) {
+		return false;
+	}
+    else {
+        return parseInt(n);
+	}
+}
 
-#fuction to validate operands
-def validate_operand():
-    operands = "+-*/%"
-    operand = input('''
+//function to validate operands
+function validate_operand() {
+    const operands = ["+", "-", "*", "/", "%"];
+    let operand = prompt(`
     Enter a valid operand
     + for addition
     - for subtraction
     * for multiplication
     / for division
     % for modulus
-    ''')
-    if operand in operands:
-        return operand
-    else:
-        print(f"your input {operand} is not a valid operand!!!")
-        validate_operand()
+    `);
+    if (operands.includes(operand)) {
+        return operand;
+	}
+	else if (operand == null) {
+		return false;
+	}
+    else {
+        alert("your input " + operand + " is not a valid operand!!!");
+        return validate_operand();
+	}
+}
 
-#function to run calculations
-def calculate(num1, num2, o):
-    if o == "+":
-        return num1 + num2
-    elif o == "-":
-        return num1 - num2
-    elif o == "*":
-        return num1 * num2
-    elif o == "/":
-        return num1 / num2
-    else:
-        return num1 % num2
+//function to run calculations
+function calculate(num1, num2, o) {
+    if (o == "+") {
+        return num1 + num2;
+    }
+	else if (o == "-") {
+        return num1 - num2;
+	}
+    else if (o == "*") {
+        return num1 * num2;
+	}
+    else if (o == "/") {
+        return num1 / num2;
+	}
+    else {
+        return num1 % num2;
+	}
+}
 
-#run the program
+//run the program
+
 main()
+
